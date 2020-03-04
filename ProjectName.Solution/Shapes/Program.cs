f77a36afe12f2a65e1ef512acdc985dda787c106
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-04 09:24:06 -0800

    Failed attempt to rename ProjectName.Solution folder due to Live Cod Share

---
+++ ProjectName.Solution/Shapes/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Shapes.Models;
+
+namespace Shapes
+{
+  public class Program
+  {
+    public static void Main()
+    {
+
+      // UI code goes here try typing
+
+    }
+  }
+}
