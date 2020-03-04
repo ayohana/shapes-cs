cd3fc3d94ded689f94f11befecea5f4b22ae07de
Jiwon <jiwon1.han@gmail.com>
2020-03-04 10:12:51 -0800

    Add Circle class and constructor

---
+++ Shapes.Solution/Shapes/Models/Circle.cs
@@ -0,0 +1,16 @@
+namespace Shapes.Models
+{
+  public class Circle
+  {
+    public double Radius { get; set; }
+    public const double Pi = 3.14;
+
+    public const double Two = 2;
+    public Circle(double radius)
+    {
+      Radius = radius; 
+    }
+
+    
+  }
+}
